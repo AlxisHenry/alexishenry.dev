e84aad94c3de10a4c807298b06e57613217895d7
AlexisHenry <alexis.henry150357@gmail.com>
2024-08-08 15:39:28 +0200

    fix: start project view

--- src/app/projects/[slug]/page.tsx
+++ src/app/projects/[slug]/page.tsx
@@ -1,12 +1,29 @@
 "use client";
 
+import Image from "next/image";
+
 import { Layout } from "@/components";
-import { ComingSoon } from "@/components/sections";
+import { ComingSoon, Title } from "@/components/sections";
+import { useProject } from "@/hooks";
 
 export default function Project() {
+  const { project } = useProject();
+
   return (
-    <Layout>
+    <Layout isView>
       <ComingSoon />
+      {/* <section>
+        <Image
+          width={960}
+          height={480}
+          src={project.thumbnail}
+          alt={project.title}
+          className={
+            "w-full min-w-[960px] object-cover rounded-2xl filter brightness-[30%]"
+          }
+        />
+        <Title content={project.title} />
+      </section> */}
     </Layout>
   );
 }
